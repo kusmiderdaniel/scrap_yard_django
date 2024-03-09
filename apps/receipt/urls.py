25e1d0cd5fe16e0be4dd5c5c5c7bfc19e3417133
kusmiderdaniel <kusmiderdaniel@gmail.com>
2024-03-09 15:21:35 +0100

    Print Receipts update

--- apps/receipt/urls.py
+++ apps/receipt/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ReceiptViewSet, ReceiptItemViewSet, ReceiptEmptyItemViewSet, generate_pdf
+from .views import ReceiptViewSet, ReceiptItemViewSet, ReceiptEmptyItemViewSet, generate_pdf, generate_receipts
 
 router = DefaultRouter()
 router.register("receipts", ReceiptViewSet, basename="receipts")
@@ -12,4 +12,5 @@ router.register("receipt-items", ReceiptItemViewSet, basename="receipt-items")
 urlpatterns = [
     path('', include(router.urls)),
     path('receipts/<int:receipt_id>/generate_pdf/', generate_pdf, name='generate_pdf'),
+    path('generate_receipts/', generate_receipts, name='generate_receipts'),
 ]
--- apps/receipt/views.py
+++ apps/receipt/views.py
@@ -1,9 +1,14 @@
 import pdfkit
 
+from datetime import datetime
+
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template.loader import get_template
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 from rest_framework import viewsets 
 from rest_framework import authentication, permissions
@@ -58,5 +63,49 @@ def generate_pdf(request, receipt_id):
 
     response = HttpResponse(pdf, content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="receipt.pdf"'
+
+    return response
+
+@api_view(['GET'])
+@authentication_classes([authentication.TokenAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def generate_receipts(request):
+
+    date_from_str = request.META.get('HTTP_X_DATE_FROM')
+    date_to_str = request.META.get('HTTP_X_DATE_TO')
+    
+    # Convert string dates to datetime objects
+    if date_from_str:
+        date_from = datetime.strptime(date_from_str, '%Y-%m-%d')
+    else:
+        date_from = None
+
+    if date_to_str:
+        date_to = datetime.strptime(date_to_str, '%Y-%m-%d')
+    else:
+        date_to = None
+
+    # Filter receipts by date range and user
+    receipts = Receipt.objects.filter(created_by=request.user)
+    if date_from:
+        receipts = receipts.filter(date__gte=date_from)
+    if date_to:
+        # Assuming date_to is inclusive
+        receipts = receipts.filter(date__lte=date_to)
+
+    receipts = receipts.order_by('created_at')
+
+    team = Team.objects.filter(created_by=request.user).first()
+
+    total_amount = sum(receipt.gross_amount for receipt in receipts)
+
+    template_name = 'receipts.html'
+
+    template = get_template(template_name)
+    html = template.render({'receipts': receipts, 'team': team, 'total_amount': total_amount, 'dateFrom': date_from_str, 'dateTo': date_to_str})
+    pdf = pdfkit.from_string(html, False)
+
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="kwity.pdf"'
 
     return response
